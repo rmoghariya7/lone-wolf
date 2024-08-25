d101705809c5b2de9bf41c6b83b5c868b7de417e
Moghariya Rahul <rahul.moghariya@zuru.com>
2024-08-25 12:25:09 +0530

    Add debug mode

--- Utils/Debug.js
+++ Utils/Debug.js
@@ -4,9 +4,10 @@ import dat from "dat.gui";
 
 export default class Debug {
   constructor() {
-    this.active = window.location.hash === "#debug";
+    this.active = window.location.pathname === "/debug";
 
     if (this.active) {
+      document.querySelector("a").style.display = "none";
       this.ui = new dat.GUI();
     }
   }
--- World/Environment.js
+++ World/Environment.js
@@ -19,11 +19,13 @@ export default class Environment {
 
   setSunLight() {
     this.sunLight = new THREE.DirectionalLight("#ffffff", 2);
+    this.ambientLight = new THREE.AmbientLight("#ffffff", 0.2);
     this.sunLight.castShadow = true;
     this.sunLight.shadow.camera.far = 15;
     this.sunLight.shadow.mapSize.set(1024, 1024);
     this.sunLight.shadow.normalBias = 0.05;
     this.sunLight.position.set(3.5, 2, -1.25);
+    this.scene.add(this.ambientLight);
     this.scene.add(this.sunLight);
 
     // Debug
@@ -31,6 +33,12 @@ export default class Environment {
       this.debugFolder
         .add(this.sunLight, "intensity")
         .name("sunLightIntensity")
+        .min(0)
+        .max(10)
+        .step(0.001);
+      this.debugFolder
+        .add(this.ambientLight, "intensity")
+        .name("ambientLightIntensity")
         .min(0)
         .max(10)
         .step(0.001);
